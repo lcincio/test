3ead7277787663716c3206d3ceeb73ca660208ac
lcincio <lcincio@pitp.ca>
2014-09-02 10:38:51 -0400

    new version

--- Test.py
+++ Test.py
@@ -1,5 +1,6 @@
 from numpy import linalg as la
 import numpy as np
+import matplotlib.pyplot as plt #our shortcut
 
 #random initialization of a tensor with comlex entries
 a = np.matrix("4 3 5; 6 7 8; 1 3 13; 7 21 9.0")
@@ -10,4 +11,17 @@ w,v = la.eig(a*b)
 
 b = w.T
 
-print("     hackers!")
+#def fib(n): 
+#    return 2*n
+    
+c = np.linspace(0,5,6)
+
+a = np.sqrt(c)
+print(a)
+
+x = numpy.linspace(0,5,20)      #create our first array
+y = x**2    #this is how Python does exponents
+
+plt.plot(x,y)           #create the plot
+plt.show()              #show the plot
+
